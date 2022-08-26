4b413ce17db4302e9d86d1037d4e11f3c8d92e4f
SkrChowdhury <sakkhor.chowdhury@gmail.com>
2022-08-26 19:56:45 +0600

    for each practice

--- map.js
+++ map.js
@@ -1,10 +1,7 @@
-
-
 let numbers = [12,34,54,32,23];
 
 const makeDouble = numbers.map(num => num * 2);
 console.log(makeDouble);
-
 
 const fiveTimes = [2,4,6,8,9].map(x => x*5);
 console.log(fiveTimes);
