a756eb4ab6da148c2e0259c8f8180e8744eff6cc
SkrChowdhury <sakkhor.chowdhury@gmail.com>
2022-08-26 20:57:40 +0600

    array reduce practice

---
+++ reduce.js
@@ -0,0 +1,12 @@
+// map, forEach, filter, find, reduce
+
+const numbers = [1, 2, 3, 4, 5];
+
+// .reduce (accumulator function, initial value)
+//accumulator function use two parameters
+const total = numbers.reduce((previous, current) => previous + current, 0);
+
+const total = numbers.reduce((previous, current) => {
+  return previous + current;
+}, 0);
+console.log(total);
